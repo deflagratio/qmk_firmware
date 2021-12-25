6b149ec20008791aa32ebb7660931a581dc7b207
deflagratio <80930087+deflagratio@users.noreply.github.com>
2021-12-25 15:02:09 -0500

    added oled

--- keyboards/lily58/keymaps/NoOled/keymap.c
+++ keyboards/lily58/keymaps/NoOled/keymap.c
@@ -81,6 +81,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 return true;
 };
 
+//OLED Screen controls
+//SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
+#ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master())
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  return rotation;
+}
+
+//Layer layer status
+void oled_task_user(void) {
+  oled_write_P(PSTR("Layer: "), false);
+
+  Switch (get_highest_layer(layer_state)){
+    case _WORKMAN:
+      oled_write_P(PSTR("WORKMAN\n"), false);
+      break;
+    case _QWERTY:
+      oled_write_P(PSTR("QWERTY\n"), false);
+      break
+    case _NAV:
+      oled_write_P(PSTR("NAV\n"), false);
+      break
+    case _NUM:
+      oled_write_P(PSTR("NUM\n"), false);
+      break
+    default:
+      oled_write_P(PSTR("Undefined"),false);
+  }
+}
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* WORKMAN
@@ -116,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |Sft/(       |   Z  |   X  |   C  |   V  |   B  |-------.    ,-------|   N  |   M  |   ,  |   .  |   /  |RIGHTCLICK|
 * `----------------------------------------------/       /     \      \----------------------------------------------'
 *             |SCREENCAP/ | DELETE| SPACE/    | /RAISE/ /       \lOWER \ |SPACE  | BACKSPACE |NUMLOCK/|
-*            |PRINTSCREEN|        |  SHIFT    |/ ENTER /         \ENTER \| SHIFT |           | WIN    |
+*            |PRINTSCREEN|        |          |/ ENTER /         \ENTER \| SHIFT |           | WIN    |
 *           `--------------------------------''-------'          '------''----------------------------'
 */
 [_QWERTY] = LAYOUT(
@@ -124,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,         KC_Q, KC_W,  KC_E,  KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
    KC_LCTRL,       KC_A, KC_S,  KC_D,  KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
    SFT_T(KC_CAPS), KC_Z, KC_X,  KC_C,  KC_V,    KC_B, LALT(KC_TAB), C(KC_TAB), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BTN1,
-     TD(TD_SCREEN_CAP), KC_DEL, SFT_T(KC_SPC), LT(RAISE,KC_ENT),        LT(LOWER,KC_ENT),  SFT_T(KC_SPC),  KC_BSPC, LGUI_T(KC_NLCK)
+     TD(TD_SCREEN_CAP), KC_DEL, KC_SPC, LT(RAISE,KC_ENT),        LT(LOWER,KC_ENT),  SFT_T(KC_SPC),  KC_BSPC, LGUI_T(KC_NLCK)
  ),
 /* NAV
  * ,-----------------------------------------------.                    ,----------------------------------------------------------.
